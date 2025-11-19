23c10267a989f5ccc21058a7af5e3dd946794fda
h4de5ing <lex109@qq.com>
2025-11-19 10:46:44 +0800

    修改配置文件+引入依赖

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "x.x.p455w0rd"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "x.x.p455w0rd"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 20
+        versionName = "2.0"
+        multiDexEnabled = true
+    }
+    signingConfigs {
+        create("release") {
+            storeFile = file("../hello_android.jks")
+            keyAlias = "android"
+            keyPassword = "android"
+            storePassword = "android"
+        }
+    }
+    buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
+        release {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+        compose = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.multidex:multidex:2.0.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.8")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.greengerong:opencsv.utils:1.2")
+    implementation("com.github.getActivity:XXPermissions:16.8")
+    implementation("com.github.h4de5ing.filepicker:filepicker:1.0-20230411")
+    implementation("androidx.biometric:biometric:1.1.0")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://gitee.com/lex1992/repository/raw/master/repository")
+        }
+    }
+}
+
+rootProject.name = "P455W0rd"
+include(":app")
