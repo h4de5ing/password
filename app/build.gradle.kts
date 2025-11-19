42779417236658875e606c71226f8a54ce57ab8f
h4de5ing <lex109@qq.com>
2025-11-19 11:27:30 +0800

    feat(app): 初始化应用主题和主界面
    - 添加了应用主题相关的颜色、字体和样式定义
    - 创建了新的主界面 MainActivity2 并配置 Jetpack Compose
    - 更新 AndroidManifest.xml 中的启动 Activity 名称
    - 在 build.gradle.kts 中添加必要的依赖库
    - 配置 libs.versions.toml 文件以管理依赖版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.activity)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
